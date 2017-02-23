2dd134c18493f33f0121d3ae8f802ddf0b73c636
Seokjun Hong <hong921122@gmail.com>
2017-02-24 01:44:45 +0900

    test

--- UnityAssetstoreAPI/Program.cs
+++ UnityAssetstoreAPI/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using UnityAssetstoreAPI.wrapper;
 
 namespace UnityAssetstoreAPI
 {
@@ -10,6 +12,11 @@ namespace UnityAssetstoreAPI
     {
         static void Main(string[] args)
         {
+            UnityAssetstoreUser a = new UnityAssetstoreUser();
+            AssetstoreUserWrapper b = a.UserLogin("", "");
+            Console.WriteLine(b.XUnitySession);
+
+            a.GetDownloadableAssets();
         }
     }
 }
