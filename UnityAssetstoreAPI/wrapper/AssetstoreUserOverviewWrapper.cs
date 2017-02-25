3f22d108fa7b31ebaa7b9d5bd54522fee6efa2c6
SeokJun Hong <hong921122@gmail.com>
2017-02-25 20:57:09 +0900

    user overview wrapper

--- UnityAssetstoreAPI/wrapper/AssetstoreUserOverviewWrapper.cs
+++ UnityAssetstoreAPI/wrapper/AssetstoreUserOverviewWrapper.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace UnityAssetstoreAPI.wrapper
 {
     class AssetstoreUserOverviewWrapper
     {
+        [JsonProperty("balance")]
+        public AssetstoreUserBalance Balance { get; set; }
+        [JsonProperty("bio")]
+        public string Bio { get; set; }
+        [JsonProperty("editable")]
+        public bool Editable { get; set; }
+        [JsonProperty("email")]
+        public string Email { get; set; }
+        [JsonProperty("himself")]
+        public bool Himself { get; set; }
+        [JsonProperty("id")]
+        public string ID { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+
+    class AssetstoreUserBalance
+    {
+        [JsonProperty("amount")]
+        public string Amount { get; set; }
+        [JsonProperty("amount_text")]
+        public string AmountText { get; set; }
+        [JsonProperty("currency")]
+        public string Currency { get; set; }
     }
 }
