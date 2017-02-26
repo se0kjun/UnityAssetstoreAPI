5910467845b5e66f1e6e6a9db4ce9f2a4358c9bd
Seokjun Hong <hong921122@gmail.com>
2017-02-27 02:52:34 +0900

    add methods for checking asset

--- UnityAssetstoreAPI/UnityAssetstoreAsset.cs
+++ UnityAssetstoreAPI/UnityAssetstoreAsset.cs
@@ -16,5 +16,47 @@ namespace UnityAssetstoreAPI
         public UnityAssetstoreAsset()
         {
         }
+
+        public AssetstoreContentOverviewWrapper GetAssetOverview(int id)
+        {
+            return UnityAssetstoreRequest.GetResponseToJson<AssetstoreContentOverviewWrapper>
+                (string.Format(ASSET_USER_OVERVIEW_URL, id), "*/*", "", "");
+        }
+
+        public List<AssetstoreContentOverviewWrapper> GetAssetsOverview(List<int> ids)
+        {
+            List<AssetstoreContentOverviewWrapper> data = new List<AssetstoreContentOverviewWrapper>();
+
+            foreach (int id in ids)
+            {
+                data.Add(UnityAssetstoreRequest.GetResponseToJson<AssetstoreContentOverviewWrapper>
+                    (string.Format(ASSET_USER_OVERVIEW_URL, id), "*/*", "", ""));
+            }
+
+            return data;
+        }
+
+        public AssetstoreContentWrapper GetAssetInfo (int id)
+        {
+            return UnityAssetstoreRequest.GetResponseToJson<AssetstoreContentWrapper>
+                (string.Format(ASSET_OVERVIEW_URL, id), "*/*", "", "");
+        }
+
+        public List<AssetstoreContentWrapper> GetAssetsInfo(List<int> ids)
+        {
+            List<AssetstoreContentWrapper> data = new List<AssetstoreContentWrapper>();
+
+            foreach (int id in ids)
+            {
+                data.Add(UnityAssetstoreRequest.GetResponseToJson<AssetstoreContentWrapper>
+                    (string.Format(ASSET_USER_OVERVIEW_URL, id), "*/*", "", ""));
+            }
+
+            return data;
+        }
+
+        public void GetDownloadAsset(int id)
+        {
+        }
     }
 }
