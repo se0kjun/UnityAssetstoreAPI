e46a770ef1e63ff5bedcf93152b7de84c9642e6b
Seokjun Hong <hong921122@gmail.com>
2017-03-28 01:48:23 +0900

    download assets

--- UnityAssetstoreAPI/UnityAssetstoreAsset.cs
+++ UnityAssetstoreAPI/UnityAssetstoreAsset.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityAssetstoreAPI.wrapper;
 using System.Net;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Security.Cryptography;
 
 namespace UnityAssetstoreAPI
 {
@@ -12,7 +15,10 @@ namespace UnityAssetstoreAPI
     {
         private const string ASSET_USER_OVERVIEW_URL = "https://www.assetstore.unity3d.com/api/ko-KR/content/user-overview/{0}.json";
         private const string ASSET_OVERVIEW_URL = "https://www.assetstore.unity3d.com/api/ko-KR/content/overview/{0}.json";
-        
+        private const string ASSET_DOWNLOAD_URL = "https://www.assetstore.unity3d.com/api/ko-KR/content/download/{0}.json";
+
+        private string packageKey;
+
         public UnityAssetstoreAsset()
         {
         }
@@ -57,6 +63,64 @@ namespace UnityAssetstoreAPI
 
         public void GetDownloadAsset(int id)
         {
+            JObject assets = JObject.FromObject(UnityAssetstoreRequest.GetResponseToJson
+                (string.Format(ASSET_DOWNLOAD_URL, id), "*/*", "", "", "GET", ""));
+
+            JToken info = assets["download"];
+            packageKey = info.Value<string>("key");
+
+            using (WebClient wc = new WebClient())
+            {
+                wc.DownloadDataAsync(new Uri(info.Value<string>("url")));
+                wc.DownloadDataCompleted += DownloadDataCompleted;
+            }
+        }
+
+        private void DownloadDataCompleted(object sender, DownloadDataCompletedEventArgs e)
+        {
+            Console.WriteLine("COMPLETE");
+            Decrypt(e.Result);
+            Console.WriteLine("DONE");
+        }
+
+        private void Decrypt(byte[] data)
+        {
+            byte[] src = ToByteArray(packageKey);
+
+            byte[] keyhash = new byte[32];
+            Array.Copy(src, 0, keyhash, 0, 32);
+            byte[] ivhash = new byte[16];
+            Array.Copy(src, 32, ivhash, 0, 16);
+
+            using (var rijndaelManaged = new RijndaelManaged
+            {
+                Key = keyhash,
+                IV = ivhash,
+                Mode = CipherMode.CBC,
+                Padding = PaddingMode.None,
+                BlockSize = 128,
+                KeySize = 256
+            })
+            {
+                using (Stream encrypted = new MemoryStream(data),
+                       decrypted = new CryptoStream(encrypted, rijndaelManaged.CreateDecryptor(keyhash, ivhash), CryptoStreamMode.Read),
+                       copy = new MemoryStream(),
+                       fileStream = File.Create("test2.unitypackage"))
+                {
+                    decrypted.CopyTo(fileStream);
+                }
+            }
+        }
+
+        public byte[] ToByteArray(String HexString)
+        {
+            int NumberChars = HexString.Length;
+            byte[] bytes = new byte[NumberChars / 2];
+            for (int i = 0; i < NumberChars; i += 2)
+            {
+                bytes[i / 2] = Convert.ToByte(HexString.Substring(i, 2), 16);
+            }
+            return bytes;
         }
     }
 }
---
+++ UnityAssetstoreAPI/wrapper/AssetstoreDownloadInfoWrapper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace UnityAssetstoreAPI.wrapper
+{
+    class AssetstoreDownloadInfoWrapper
+    {
+        [JsonProperty("link")]
+        public AssetstoreLinkWrapper Link;
+        [JsonProperty("progress")]
+        public int Progress;
+        [JsonProperty("filename_safe_package_name")]
+        public string FileName;
+        [JsonProperty("key")]
+        public string Key;
+        [JsonProperty("filename_safe_category_name")]
+        public string CategoryName;
+        [JsonProperty("url")]
+        public string URL;
+        [JsonProperty("filename_safe_publisher_name")]
+        public string PublisherName;
+        [JsonProperty("id")]
+        public string ID;
+    }
+
+    class AssetstoreLinkWrapper
+    {
+        [JsonProperty("id")]
+        public string ID;
+        [JsonProperty("type")]
+        public string Type;
+    }
+}
